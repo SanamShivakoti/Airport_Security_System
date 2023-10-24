09ce55d7e779b181119f617b0f0019c2fc01270d
sanam <np03cs4m210078@heraldcollege.edu.np>
2023-10-24 08:01:14 +0545

    Remove the create method from RoleSerializer class.

--- backend/Airport_Security/serializers.py
+++ backend/Airport_Security/serializers.py
@@ -8,18 +8,7 @@ class RoleSerializer(serializers.ModelSerializer):
         model = Role
         fields = '__all__'
         
-        def create(self, request, *args, **kwargs):
-            role_id = request.data.get('role_id')
-            role_name = request.data.get('role_id')
-        
-                    # Check if the role_id is empty
-            if not role_id:
-                raise serializers.ValidationError('Role is required')
 
-            if not role_name:
-                raise serializers.ValidationError('Role name is required')
-            
-            return super().create(request, *args, **kwargs)
         
 class AdminSerializer(serializers.ModelSerializer):
     class Meta:
