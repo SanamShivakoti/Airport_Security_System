7157d8c9f7181d4e3ecb8a59d1cd0a397dc0e4d3
sanam <np03cs4m210078@heraldcollege.edu.np>
2023-10-31 18:54:59 +0545

    Add hyperlink to view passengers details and setting the button width.

--- frontend/src/Admin/Passengers/Passengers_details.jsx
+++ frontend/src/Admin/Passengers/Passengers_details.jsx
@@ -75,7 +75,7 @@ function PassengersRegistration() {
                 id="email"
                 placeholder="Email"
                 autoComplete="given-name"
-                className="block  my-px w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                className="block  my-px w-full rounded-md border-0 py-1.5 w-[36rem]text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               />
             </div>
 
@@ -299,15 +299,17 @@ function PassengersRegistration() {
           </p>
 
           <div className="desktop:mt-7 laptop:mt-14 tablet:mt-14 flex items-center justify-end gap-x-6 laptop:px-32 desktop:px-40  tablet:px-24">
-            <button
-              type="button"
-              className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-            >
-              View Passengers
-            </button>
+            <a href="View/details">
+              <button
+                type="button"
+                className="rounded-md bg-indigo-600 w-[39rem] px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                View Passengers
+              </button>
+            </a>
             <button
               type="submit"
-              className="rounded-md bg-lime-700 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-lime-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-lime-700"
+              className="rounded-md bg-lime-700 w-[39rem] px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-lime-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-lime-700"
             >
               Register Passengers
             </button>
