4de648711398804b8605273c351cb32c03d77ef1
sanam <np03cs4m210078@heraldcollege.edu.np>
2023-11-02 20:37:45 +0545

    Create form for OTP verification for change password by Users. And check the Success and error messages as well as set and check to Redirect the set new password page after verified the OTP.

---
+++ frontend/src/Users/Settings/change_password/verify_otp.jsx
@@ -0,0 +1,79 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+function OtpVerification() {
+  const [otp, setOtp] = useState("");
+  const [verificationStatus, setVerificationStatus] = useState("");
+  const [otpSent, setOtpSent] = useState(false); // Track whether OTP has been sent
+  const navigate = useNavigate();
+
+  const handleOtpChange = (e) => {
+    setOtp(e.target.value);
+  };
+
+  const handleVerify = () => {
+    const mockOtp = "123456";
+    if (otp === mockOtp) {
+      setVerificationStatus("Verified!");
+      navigate("/User/Settings/set/newpassword");
+    } else {
+      setVerificationStatus("Incorrect OTP. Please try again.");
+    }
+  };
+
+  const handleResendOtp = () => {
+    setOtpSent(true);
+    setVerificationStatus("OTP of 6 digits has been sent to your email.");
+  };
+
+  return (
+    <div className="max-w-md mx-auto">
+      <h1 className="text-3xl font-semibold text-center my-4">OTP Verification</h1>
+      <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2">
+            {otpSent
+              ? "Please enter the OTP sent to your email"
+              : "Click the button to receive the OTP via email"}
+          </label>
+          {otpSent ? (
+            <>
+              <input
+                type="text"
+                placeholder="Enter OTP"
+                value={otp}
+                onChange={handleOtpChange}
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:ring-2 focus:ring-inset focus:ring-indigo-600" 
+              />
+              <p className="text-center text-blue-600 cursor-pointer underline mt-2" onClick={handleResendOtp}>
+                Resend OTP
+              </p>
+            </>
+          ) : (
+            <button
+              onClick={handleResendOtp}
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            >
+              Receive OTP
+            </button>
+          )}
+        </div>
+        {verificationStatus && (
+          <div className="text-center">
+            <button
+              onClick={handleVerify}
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            >
+              Verify
+            </button>
+          </div>
+        )}
+        {verificationStatus && (
+          <p className="mt-4 text-center text-red-600">{verificationStatus}</p>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default OtpVerification;
