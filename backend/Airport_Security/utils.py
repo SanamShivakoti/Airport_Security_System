27dfd53cb32ebd04f53e775ae4499ce466597177
sanam <np03cs4m210078@heraldcollege.edu.np>
2023-11-05 00:21:27 +0545

    Remove the 'generate_role_id' method which is used to generate for role id. But now 'role' is used in User model table only so, it is not necessary to generate role id.

--- backend/Airport_Security/utils.py
+++ backend/Airport_Security/utils.py
@@ -7,10 +7,3 @@ def generate_user_id():
     # Add "U" as a prefix to the random number
     user_id = f'U{random_number}'
     return user_id
-
-def generate_role_id():
-    # Generate a 4-digit random number
-    random_number = ''.join(random.choices(string.digits, k=4))
-    # Add "R" as a prefix to the random number
-    role_id = f'R{random_number}'
-    return role_id
