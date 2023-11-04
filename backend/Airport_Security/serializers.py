24145d8148fccd256058e77eb476342aeafabb9c
sanam <np03cs4m210078@heraldcollege.edu.np>
2023-11-05 00:18:07 +0545

    Remove the 'serialzer' methods of Role and Admin. And only used as 'UserSerializer'.

--- backend/Airport_Security/serializers.py
+++ backend/Airport_Security/serializers.py
@@ -1,21 +1,8 @@
 from rest_framework_json_api import serializers
-from .models import Role, Admin, User
+from .models import User
 from django.utils import timezone
 
-
-class RoleSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Role
-        fields = '__all__'
-        
-
-        
-class AdminSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Admin
-        fields = '__all__'
-        
-        
+       
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
