a8fff1dc13b623ba6e33f1677e7af4ac6f2ed1ef
sanam <np03cs4m210078@heraldcollege.edu.np>
2023-11-06 00:30:34 +0545

    Add the class for user registrations view for 'users registration' and written the logic for it in view

--- backend/Airport_Security/views.py
+++ backend/Airport_Security/views.py
@@ -1,16 +1,25 @@
-from rest_framework import viewsets, serializers
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.views import APIView
 from .models import User
-from .serializers import UserSerializer
+from .serializers import UserRegistrationSerializer
 from rest_framework.response import Response
 from rest_framework import status
 
 import json
 
+class UserRegistrationView(APIView):
+    def post(self, request, format=None):
+        serializer = UserRegistrationSerializer(data = request.data)
+        serializer.is_valid(raise_exception=True)
+        email = serializer.validated_data['email']
+        
+        if User.objects.filter(email=email).exists():
+            return Response({"error": "User with the same user_id or email already exists."})
+        
+        user = serializer.save()
+        return Response({'msg' : 'Registration Successful'}, status=status.HTTP_201_CREATED)
 
-# Create a UserViewSet
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
       
 
         
