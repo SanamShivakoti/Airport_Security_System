85d55038ac34474253d62002deb137fdc4c8e774
sanam <np03cs4m210078@heraldcollege.edu.np>
2023-11-07 00:15:55 +0545

    Created the 'loginUserSerializer' for login user and write logic for it as well for user profile view.

--- backend/Airport_Security/serializers.py
+++ backend/Airport_Security/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
-from .models import User
-from django.core.validators import RegexValidator      
+from .models import User    
+
+# serializer for User Registration
 class UserRegistrationSerializer(serializers.ModelSerializer):
 
     password2 = serializers.CharField(style={'input_type':'password'}, write_only=True)
@@ -25,4 +26,16 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
     def create(self, validate_data):
         return User.objects.create_user(**validate_data)
         
-    
+
+# serializer for Login User
+class LoginUserSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(max_length=255)
+    class Meta:
+        model = User
+        fields = ['email','password']
+
+
+class UserProfileSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = User
+    fields = ['user_id', 'first_name','middle_name','last_name','mobile_number','role','email']
