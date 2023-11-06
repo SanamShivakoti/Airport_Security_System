699d25913c4c2857ea898ab225ec1343f89add4f
sanam <np03cs4m210078@heraldcollege.edu.np>
2023-11-07 00:20:39 +0545

    Set the routes for login user and profile view of logged in user.

--- backend/Airport_Security/urls.py
+++ backend/Airport_Security/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from Airport_Security.views import UserRegistrationView
+from Airport_Security.views import UserRegistrationView, LoginUserView, UserProfileView
 urlpatterns = [
     path('register/user/', UserRegistrationView.as_view(), name = 'register'),
+    path('login/', LoginUserView.as_view(), name='login'),
+    path('profile/', UserProfileView.as_view(), name='profile'),
 
 ]
