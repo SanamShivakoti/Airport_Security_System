95100d67323b6679b0a5040c89aadefa8c5cc371
sanam <np03cs4m210078@heraldcollege.edu.np>
2023-11-07 23:56:59 +0545

    Created three classes, for Sending email, for verifying OTP, and after OTP verification for changing password.

--- backend/Airport_Security/views.py
+++ backend/Airport_Security/views.py
@@ -10,6 +10,12 @@ from Airport_Security.renderers import UserRenderer
 from rest_framework_simplejwt.tokens import RefreshToken
 from Airport_Security.serializers import UserProfileSerializer
 from rest_framework.permissions import IsAuthenticated
+from Airport_Security.utils import generate_otp, Util
+from django.utils import timezone
+from datetime import timedelta
+from Airport_Security.serializers import OTPVerificationSerializer, PasswordResetSerializer
+from django.urls import reverse
+from django.http import HttpResponseRedirect, HttpResponse
 
 # Generate Token Manually
 def get_tokens_for_user(user):
@@ -61,6 +67,81 @@ class UserProfileView(APIView):
   def get(self, request, format=None):
     serializer = UserProfileSerializer(request.user)
     return Response(serializer.data, status=status.HTTP_200_OK)
+  
+
+
+class SendOtpResetEmailView(APIView):
+    renderer_classes = [UserRenderer]
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, format=None):
+            user = request.user
+            otp = generate_otp()
+            user.otp = otp
+            user.otp_timestamp = timezone.now()
+            user.save()
+            subject = 'Password Reset OTP'
+            message = f'Your OTP is: {otp}'
+            to_email = user.email
+
+            email_data = {
+                'subject': subject,
+                'body': message,
+                'to_email': to_email,
+            }
+
+            Util.send_email(email_data)
+
+            return Response({'msg': 'OTP sent via email'}, status=status.HTTP_200_OK)
+    
+
+
+class VerifyOtpView(APIView):
+    renderer_classes = [UserRenderer]
+    permission_classes = [IsAuthenticated]
+    def post(self, request, format=None):
+        serializer = OTPVerificationSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        otp = serializer.validated_data['otp']
+        
+
+        user = request.user
+        if user.otp == otp:
+            time_limit = timedelta(minutes=5)  # Adjust the time limit as needed
+            if user.otp_timestamp and timezone.now() - user.otp_timestamp < time_limit:
+                user.otp = None
+                user.otp_timestamp = None
+                user.save()
+
+                password_change_url = reverse('password_reset')
+                return HttpResponseRedirect(password_change_url)
+            else:
+                return Response({'error': 'OTP has expired'}, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            return Response({'error': 'Invalid OTP'}, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+class OTPVerifiedPasswordResetView(APIView):
+
+    def OTPVerifiedPasswordReset(request):
+        user = request.user
+
+        if user.otp:
+            return HttpResponseRedirect(reverse('otp_verification'))
+        else:
+            if request.method =='POST':
+                serializer = PasswordResetSerializer(data=request.POST)
+                if serializer.is_valid():
+                    new_password = serializer.validated_data['password']
+                    user.set_password(new_password)
+                    user.save()
+
+                    return Response({'error':'Password changed successfully'}, status=status.HTTP_200_OK) 
+                else:
+                    return Response({'error': 'Password changed Unsuccessful'}, status=status.HTTP_400_BAD_REQUEST)
+                
+            return Response({'error':'!! Sorry, something went wrong !!'})
 
 
 
