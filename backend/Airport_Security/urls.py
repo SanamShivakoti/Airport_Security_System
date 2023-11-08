470d14d1f24c3fa89720805c189c515da977efc3
sanam <np03cs4m210078@heraldcollege.edu.np>
2023-11-08 23:49:02 +0545

    Defined the routes for get all the Lists of Users, Update Users, and Delete Users.

--- backend/Airport_Security/urls.py
+++ backend/Airport_Security/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from Airport_Security.views import UserRegistrationView, LoginUserView, UserProfileView, OTPVerifiedPasswordResetView, VerifyOtpView, SendOtpResetEmailView
+from Airport_Security.views import UserRegistrationView, LoginUserView, UserProfileView, OTPVerifiedPasswordResetView, UserView, UpdateUserView, DeleteUserView,VerifyOtpView, SendOtpResetEmailView
 urlpatterns = [
     path('register/user/', UserRegistrationView.as_view(), name = 'register'),
     path('login/', LoginUserView.as_view(), name='login'),
@@ -7,5 +7,8 @@ urlpatterns = [
     path('send_otp_email/', SendOtpResetEmailView.as_view(), name='send_otp_email'),
     path('otp_verification/', VerifyOtpView.as_view(), name='otp_verification'),
     path('password_reset/', OTPVerifiedPasswordResetView.as_view(), name='password_reset'),
+    path('users_list/', UserView.as_view(), name='users_list'),
+    path('user_update/', UpdateUserView.as_view(), name='user_update'),
+    path('user_delete/', DeleteUserView.as_view(), name='user_delete'),
 
 ]
