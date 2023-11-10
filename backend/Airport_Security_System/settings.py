1d25de86ab777abe67f18bfdbe916683e128a232
sanam <np03cs4m210078@heraldcollege.edu.np>
2023-11-10 22:31:16 +0545

    Configured the 'django cors-header' package to established connection between frontend and backend.

--- backend/Airport_Security_System/settings.py
+++ backend/Airport_Security_System/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework',
     'Airport_Security',
     'rest_framework_simplejwt',
@@ -47,6 +48,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -169,3 +171,7 @@ SIMPLE_JWT = {
     "JTI_CLAIM": "jti",
 }
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+]
