1c1a784df696ae1427d5fd377ebb64a5dd10e34c
sanam <np03cs4m210078@heraldcollege.edu.np>
2023-11-14 21:48:45 +0545

    Created a function 'PrivateRoute' and built the logic works as middleware (To access denied by search routes directly from URL) the user should get logged to access other pages.

--- frontend/src/routers/routes.js
+++ frontend/src/routers/routes.js
@@ -1,20 +1,30 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 // import for Admin control to setup router
 import Sidebar from "../Admin/Dashboard/Components";
 // import for Users control to setup router
 import Usersidebar from "../Users/Dashboard/Components";
 // import for login page control to setup router
 import { Login } from "../login";
+import { useSelector } from "react-redux/es/hooks/useSelector";
+
+function PrivateRoute({ element, authenticated, redirectTo }) {
+  return authenticated ? (
+    element
+  ) : (
+    <Navigate to={redirectTo} replace state={{ from: window.location.pathname }} />
+  );
+}
 
 function Url() {
+  const {access_token} = useSelector(state => state.auth)
   return (
     <BrowserRouter>
       <Routes>
         {/* login page route */}
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={<PrivateRoute element={<Login />} authenticated={!access_token} redirectTo="/Admin/dashboard/" />} />
 
         {/* Admin controls route */}
-        <Route path="/Admin/*" element={<Sidebar />} />
+        <Route path="/Admin/*"  element={<PrivateRoute element={<Sidebar />} authenticated={!!access_token} redirectTo="/" />} />
 
         {/* Users controls route */}
         <Route path="/User/*" element={<Usersidebar />} />
