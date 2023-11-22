842d097d87d693fcf459ce9cee1c80d3cfb2524e
sanam <np03cs4m210078@heraldcollege.edu.np>
2023-11-22 23:00:41 +0545

    Written the logic to display users list in the table.

--- frontend/src/Admin/User/view_users/view_users.jsx
+++ frontend/src/Admin/User/view_users/view_users.jsx
@@ -1,12 +1,23 @@
 import React, { useState } from "react";
+import { useGetUsersQuery } from "../../../services/userAuthApi";
+import { getToken } from "../../../services/LocalStorageService";
 
 function ViewUsers() {
   const [searchTerm, setSearchTerm] = useState("");
-
+  const {access_token}  = getToken();
+  const { data: users = [], error, isLoading } = useGetUsersQuery({access_token});
   // Function to handle search input change
   const handleSearchChange = (e) => {
     setSearchTerm(e.target.value);
   };
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error loading users: {error}</div>;
+  }
 
   return (
     <div>
@@ -44,62 +55,42 @@ function ViewUsers() {
                 <th className="px-1 py-2  w-1/6text-center bg-gray-300">
                   User ID
                 </th>
-                <th className="px-1 py-2 text-center bg-gray-300">Password</th>
+                <th className="px-1 py-2 text-center bg-gray-300">Role</th>
+                <th className="px-1 py-2 text-center bg-gray-300">Status</th>
                 <th className="px-1 py-2 text-center bg-gray-300">Edit</th>
                 <th className="px-1 py-2 text-center bg-gray-300">Delete</th>
               </tr>
             </thead>
             <tbody>
-              <tr
-                className="table-row group hover:bg-yellow-200 hover:cursor-pointer"
-                data-row-id="1"
-              >
-                <td className="border px-1 py-1 w-1/12">1</td>
-                <td className="border px-1 py-1 w-1/4">Row 1, Cell 2</td>
-                <td className="border px-1 py-1 w-1/6">Row 1, Cell 3</td>
-                <td className="border px-1 py-1">Row 1, Cell 4</td>
-                <td className="border px-1 py-1">
-                  <button
-                    type="button"
-                    className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                  >
-                    Edit
-                  </button>
-                </td>
-                <td className="border px-1 py-1">
-                  <button
-                    type="submit"
-                    className="rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600"
-                  >
-                    Delete
-                  </button>
-                </td>
-              </tr>
-              <tr
-                className="table-row group hover:bg-yellow-200 hover:cursor-pointer"
-                data-row-id="2"
-              >
-                <td className="border px-1 py-1 w-1/12">2</td>
-                <td className="border px-1 py-1 w-1/4">Row 2, Cell 2</td>
-                <td className="border px-1 py-1 w-1/6">Row 2, Cell 3</td>
-                <td className="border px-1 py-1">Row 2, Cell 4</td>
-                <td className="border px-1 py-1">
-                  <button
-                    type="button"
-                    className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                  >
-                    Edit
-                  </button>
-                </td>
-                <td className="border px-1 py-1">
-                  <button
-                    type="submit"
-                    className="rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600"
-                  >
-                    Delete
-                  </button>
-                </td>
-              </tr>
+              {users.map((user, index) => (
+                <tr
+                  key={user.user_id}
+                  className="table-row group hover:bg-yellow-200 hover:cursor-pointer"
+                  data-row-id={user.id}
+                >
+                  <td className="border px-1 py-1 w-1/12">{index + 1}</td>
+                  <td className="border px-1 py-1 w-1/4">{`${user.first_name} ${user.middle_name} ${user.last_name}`}</td>
+                  <td className="border px-1 py-1 w-1/12">{user.user_id}</td>
+                  <td className="border px-1 py-1 w-1/12">{user.role}</td>
+                  <td className="border px-1 py-1 w-1/12">{user.status}</td>
+                  <td className="border px-1 py-1">
+                    <button
+                      type="button"
+                      className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                    >
+                      Edit
+                    </button>
+                  </td>
+                  <td className="border px-1 py-1">
+                    <button
+                      type="submit"
+                      className="rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600"
+                    >
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              ))}
             </tbody>
           </table>
         </div>
