0b9dd22cb1f7a1687fdece8fb29c10635eb62c40
sanam <np03cs4m210078@heraldcollege.edu.np>
2023-11-23 22:52:54 +0545

    Written the logic for filter the users from the search field and display in the table. Also, Set the delete User functionality after hit the Delete button of table.

--- frontend/src/Admin/User/view_users/view_users.jsx
+++ frontend/src/Admin/User/view_users/view_users.jsx
@@ -1,14 +1,52 @@
-import React, { useState } from "react";
+import React, { useState, useMemo } from "react";
 import { useGetUsersQuery } from "../../../services/userAuthApi";
 import { getToken } from "../../../services/LocalStorageService";
+import axios from "axios";
 
 function ViewUsers() {
-  const [searchTerm, setSearchTerm] = useState("");
-  const {access_token}  = getToken();
-  const { data: users = [], error, isLoading } = useGetUsersQuery({access_token});
+  const [userIdSearch, setUserIdSearch] = useState("");
+  const [serverError, setServerError] = useState({});
+  const [successMessage, setSuccessMessage] = useState({});
+  const { access_token } = getToken();
+
+  // Fetch users data using the useGetUsersQuery hook
+  const { data: users = [], error, isLoading } = useGetUsersQuery({
+    access_token,
+  });
+
+  // Memoized filtering of users based on search input
+  const filteredUsers = useMemo(() => {
+    return users.filter((user) =>
+      user.user_id.toLowerCase().includes(userIdSearch.toLowerCase())
+    );
+  }, [users, userIdSearch]);
+
   // Function to handle search input change
   const handleSearchChange = (e) => {
-    setSearchTerm(e.target.value);
+    setUserIdSearch(e.target.value);
+  };
+
+  const handleDelete = async (user_id) => {
+    try {
+      const response = await axios.delete(
+        `http://127.0.0.1:8000/api/admin/user_delete/${user_id}`,
+        {
+          headers: {
+            Authorization: `Bearer ${access_token}`,
+          },
+        }
+      );
+
+      if (response.data.error) {
+        console.error("Error deleting user:", response.error);
+        setServerError(response.data.error);
+      } else if (response.data.msg) {
+        setSuccessMessage(response.data.msg);
+        window.location.reload();
+      }
+    } catch (error) {
+      console.error("Error deleting user:", error);
+    }
   };
 
   if (isLoading) {
@@ -26,33 +64,26 @@ function ViewUsers() {
       </div>
       <div className="pl-64 pr-2">
         <div className="mb-4 mt-10">
-          {/* Add margin-bottom to create space between search and table */}
           {/* Search input */}
           <input
             type="text"
-            placeholder="Search by User ID or User Name"
-            className="border rounded-md px-2 py-2 w-1/4" // Adjust width as needed
-            value={searchTerm}
+            name="user_id"
+            placeholder="Search by User ID"
+            className="border rounded-md px-2 py-2 w-1/4"
+            value={userIdSearch}
             onChange={handleSearchChange}
           />
-          {/* Search button */}
-          <button
-            type="button"
-            className="rounded-md bg-blue-500 text-white px-2 py-2 ml-2 w-44" // Add margin-left for spacing
-          >
-            Search
-          </button>
         </div>
 
         <div className="overflow-x-auto overflow-y-auto">
           <table className="min-w-full border-collapse table-auto">
             <thead>
               <tr>
-                <th className="px-1 py-2  w-1/12text-left bg-gray-300">SN</th>
-                <th className="px-1 py-2  w-1/4text-center bg-gray-300">
+                <th className="px-1 py-2 w-1/12 text-left bg-gray-300">SN</th>
+                <th className="px-1 py-2 w-1/4 text-center bg-gray-300">
                   User Name
                 </th>
-                <th className="px-1 py-2  w-1/6text-center bg-gray-300">
+                <th className="px-1 py-2 w-1/6 text-center bg-gray-300">
                   User ID
                 </th>
                 <th className="px-1 py-2 text-center bg-gray-300">Role</th>
@@ -62,7 +93,7 @@ function ViewUsers() {
               </tr>
             </thead>
             <tbody>
-              {users.map((user, index) => (
+              {filteredUsers.map((user, index) => (
                 <tr
                   key={user.user_id}
                   className="table-row group hover:bg-yellow-200 hover:cursor-pointer"
@@ -85,6 +116,7 @@ function ViewUsers() {
                     <button
                       type="submit"
                       className="rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600"
+                      onClick={() => handleDelete(user.user_id)}
                     >
                       Delete
                     </button>
