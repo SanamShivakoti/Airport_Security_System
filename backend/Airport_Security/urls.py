df716d1fd88362f0f113af1559832bca61577cf4
sanam <np03cs4m210078@heraldcollege.edu.np>
2023-11-26 00:16:06 +0545

    Set the name of AdminProfileView that was changed.

--- backend/Airport_Security/urls.py
+++ backend/Airport_Security/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from Airport_Security.views import UserRegistrationView, LoginUserView, UserProfileView, OTPVerifiedPasswordResetView, UserView, UpdateUserView, DeleteUserView,VerifyOtpView, SendOtpResetEmailView, FilterUserView
+from Airport_Security.views import UserRegistrationView, LoginUserView, AdminProfileView, OTPVerifiedPasswordResetView, UserView, UpdateUserView, DeleteUserView,VerifyOtpView, SendOtpResetEmailView, FilterUserView
 urlpatterns = [
     path('register/user/', UserRegistrationView.as_view(), name = 'register'),
     path('login/', LoginUserView.as_view(), name='login'),
-    path('profile/', UserProfileView.as_view(), name='profile'),
+    path('profile/', AdminProfileView.as_view(), name='profile'),
     path('send_otp_email/', SendOtpResetEmailView.as_view(), name='send_otp_email'),
     path('otp_verification/', VerifyOtpView.as_view(), name='otp_verification'),
     path('password_reset/', OTPVerifiedPasswordResetView.as_view(), name='password_reset'),
