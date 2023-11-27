ab3f0644a863f9ea9b774d522674749f576fed42
sanam <np03cs4m210078@heraldcollege.edu.np>
2023-11-27 23:44:26 +0545

    feat:Set the route for Change password for Admin.

--- backend/Airport_Security/urls.py
+++ backend/Airport_Security/urls.py
@@ -1,15 +1,15 @@
 from django.urls import path
-from Airport_Security.views import UserRegistrationView, LoginUserView, AdminProfileView, OTPVerifiedPasswordResetView, UserView, UpdateUserView, DeleteUserView,VerifyOtpView, SendOtpResetEmailView, FilterUserView
+from Airport_Security.views import UserRegistrationView, LoginUserView, AdminProfileView, OTPVerifiedPasswordResetView, UserView, UpdateUserView, DeleteUserView,VerifyOtpView, SendOtpResetEmailView, FilterUserView, AdminChangePasswordView
 urlpatterns = [
     path('register/user/', UserRegistrationView.as_view(), name = 'register'),
     path('login/', LoginUserView.as_view(), name='login'),
     path('profile/', AdminProfileView.as_view(), name='profile'),
     path('send_otp_email/', SendOtpResetEmailView.as_view(), name='send_otp_email'),
     path('otp_verification/', VerifyOtpView.as_view(), name='otp_verification'),
-    path('password_reset/', OTPVerifiedPasswordResetView.as_view(), name='password_reset'),
     path('users_list/', UserView.as_view(), name='users_list'),
     path('user_update/<str:user_id>/', UpdateUserView.as_view(), name='user_update'),
     path('user_delete/<str:user_id>/', DeleteUserView.as_view(), name='user_delete'),
     path('user_filter/<str:user_id>/', FilterUserView.as_view(), name='user_filter'),
+    path('change_password/', AdminChangePasswordView.as_view(), name='change_password'),
 
 ]
