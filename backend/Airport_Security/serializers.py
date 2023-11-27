f3e15b57a6c0dae03f9baedc54d365438d11f591
sanam <np03cs4m210078@heraldcollege.edu.np>
2023-11-27 23:42:38 +0545

    feat:Add the change password view in backend for Admin control.

--- backend/Airport_Security/serializers.py
+++ backend/Airport_Security/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import User    
+from django.contrib.auth.hashers import make_password
 
 # serializer for User Registration
 class UserRegistrationSerializer(serializers.ModelSerializer):
@@ -44,7 +45,7 @@ class UserProfileSerializer(serializers.ModelSerializer):
 class OTPVerificationSerializer(serializers.Serializer):
     otp = serializers.CharField(max_length=6, required=True)
 
-class PasswordResetSerializer(serializers.Serializer):
+class PasswordResetSerializer(serializers.ModelSerializer):
     password = serializers.CharField(style={'input_type': 'password'}, write_only=True, required=True)
     password2 = serializers.CharField(style={'input_type': 'password'}, write_only=True, required=True)
 
@@ -63,3 +64,29 @@ class FilterUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = '__all__'
+
+class AdminChangePasswordSerializer(serializers.ModelSerializer):
+    password2 = serializers.CharField(style={'input_type':'password'}, write_only=True)
+    
+    class Meta:
+        model = User
+        fields = ['password', 'password2']
+        extra_kwargs={
+            'password':{'write_only':True}
+        }
+
+
+    def validate(self, attrs):
+
+        password = attrs.get('password')
+        password2 = attrs.get('password2')
+
+        if password != password2:
+            raise serializers.ValidationError("Password and Confirm Password doen't match.")
+        else:
+             attrs['password'] = make_password(attrs['password'])
+
+        return attrs
+    
+    def create(self, validate_data):
+        return User.objects.create_user(**validate_data)
