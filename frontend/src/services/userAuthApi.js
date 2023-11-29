08aef1690e2726518d70c94b75429a4798899f0a
sanam <np03cs4m210078@heraldcollege.edu.np>
2023-11-29 21:49:51 +0545

    tested: Modified and tested the endpoints.

--- frontend/src/services/userAuthApi.js
+++ frontend/src/services/userAuthApi.js
@@ -1,154 +1,144 @@
 // Need to use the React-specific entry point to import createApi
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 // Define a service using a base URL and expected endpoints
 export const userAuthApi = createApi({
-  reducerPath: 'userAuthApi',
-  baseQuery: fetchBaseQuery({ baseUrl: 'http://127.0.0.1:8000/api/admin/'}),
+  reducerPath: "userAuthApi",
+  baseQuery: fetchBaseQuery({ baseUrl: "http://127.0.0.1:8000/api/admin/" }),
   endpoints: (builder) => ({
     registerUser: builder.mutation({
-      query:({ actualData, access_token })=>{
-          return{
-              url:'register/user/',
-              method:'POST',
-              body: actualData,
-              headers:{
-                'authorization':`Bearer ${access_token}`,
-                'Content-type':'application/json'
-                
-              }
-          }
-      }
+      query: ({ actualData, access_token }) => {
+        return {
+          url: "register/user/",
+          method: "POST",
+          body: actualData,
+          headers: {
+            authorization: `Bearer ${access_token}`,
+            "Content-type": "application/json",
+          },
+        };
+      },
     }),
     loginUser: builder.mutation({
-      query:(user)=>{
-        return{
-          url:'login/',
-          method: 'POST',
+      query: (user) => {
+        return {
+          url: "login/",
+          method: "POST",
           body: user,
-          headers:{
-            'Content-type':'application/json'
-          }
-        }
-      }
+          headers: {
+            "Content-type": "application/json",
+          },
+        };
+      },
     }),
     getUsers: builder.query({
-      query:({ access_token })=>{
-          return{
-              url:'users_list/',
-              method:'GET',
-              headers:{
-                'authorization':`Bearer ${access_token}`
-              }
-          }
-      }
+      query: ({ access_token }) => {
+        return {
+          url: "users_list/",
+          method: "GET",
+          headers: {
+            authorization: `Bearer ${access_token}`,
+          },
+        };
+      },
     }),
     AdminProfileView: builder.query({
-      query:({ access_token })=>{
-          return{
-              url:'profile/',
-              method:'GET',
-              headers:{
-                'authorization':`Bearer ${access_token}`
-              }
-          }
-      }
+      query: ({ access_token }) => {
+        return {
+          url: "profile/",
+          method: "GET",
+          headers: {
+            authorization: `Bearer ${access_token}`,
+          },
+        };
+      },
     }),
     filterUsers: builder.query({
-      query:({ user_id,access_token})=>{
-  
-          return{
-              url:`user_filter/${user_id}/`,
-              method:'GET',
-              headers:{
-                'authorization':`Bearer ${access_token}`,
-                'Content-type':'application/json'
-                
-              }
-              
-          }
-          
-      }
+      query: ({ user_id, access_token }) => {
+        return {
+          url: `user_filter/${user_id}/`,
+          method: "GET",
+          headers: {
+            authorization: `Bearer ${access_token}`,
+            "Content-type": "application/json",
+          },
+        };
+      },
     }),
     DeleteUser: builder.mutation({
-      query:({user_id, access_token})=>{
-         
-        return{
-          url:`user_delete/${user_id}/`,
-          method: 'DELETE',
-          headers:{
-            'authorization':`Bearer ${access_token}`
-          }
-        }
-      }
+      query: ({ user_id, access_token }) => {
+        return {
+          url: `user_delete/${user_id}/`,
+          method: "DELETE",
+          headers: {
+            authorization: `Bearer ${access_token}`,
+          },
+        };
+      },
     }),
     updateUser: builder.mutation({
-      query:({ user_id, actualData, access_token })=>{
-          return{
-              url:`user_update/${user_id}/`,
-              method:'PATCH',
-              body: actualData,
-              headers:{
-                'authorization':`Bearer ${access_token}`,
-                'Content-type':'application/json'
-                
-              }
-          }
-      }
+      query: ({ user_id, actualData, access_token }) => {
+        return {
+          url: `user_update/${user_id}/`,
+          method: "PATCH",
+          body: actualData,
+          headers: {
+            authorization: `Bearer ${access_token}`,
+            "Content-type": "application/json",
+          },
+        };
+      },
     }),
     SendOTP: builder.mutation({
-      query:({ access_token })=>{
-          return{
-              url:'send_otp_email/',
-              method:'POST',
-              headers:{
-                'authorization':`Bearer ${access_token}`,
-                'Content-type':'application/json'
-                
-              }
-          }
-      }
+      query: ({ access_token }) => {
+        return {
+          url: "send_otp_email/",
+          method: "POST",
+          headers: {
+            authorization: `Bearer ${access_token}`,
+            "Content-type": "application/json",
+          },
+        };
+      },
     }),
     VerifyOTP: builder.mutation({
-      query:({ access_token, otp})=>{
-          return{
-              url:'otp_verification/',
-              method:'POST',
-              body:{otp},
-              headers:{
-                'authorization':`Bearer ${access_token}`,
-                'Content-type':'application/json'
-                
-              }
-          }
-      }
+      query: ({ access_token, otp }) => {
+        return {
+          url: "otp_verification/",
+          method: "POST",
+          body: { otp },
+          headers: {
+            authorization: `Bearer ${access_token}`,
+            "Content-type": "application/json",
+          },
+        };
+      },
     }),
     ResetPassword: builder.mutation({
-      query:({ actualData, access_token })=>{
-          return{
-              url:'change_password/',
-              method:'PUT',
-              body:actualData,
-              headers:{
-                'authorization':`Bearer ${access_token}`,
-                'Content-type':'application/json'
-                
-              }
-          }
-      }
+      query: ({ actualData, access_token }) => {
+        return {
+          url: "change_password/",
+          method: "PUT",
+          body: actualData,
+          headers: {
+            authorization: `Bearer ${access_token}`,
+            "Content-type": "application/json",
+          },
+        };
+      },
     }),
-
   }),
-})
+});
 
 export const {
-  useRegisterUserMutation, 
-  useLoginUserMutation, 
+  useRegisterUserMutation,
+  useLoginUserMutation,
   useGetUsersQuery,
-  useAdminProfileViewQuery, 
+  useAdminProfileViewQuery,
   useFilterUsersQuery,
-  useUpdateUserMutation, 
+  useUpdateUserMutation,
   useDeleteUserMutation,
   useSendOTPMutation,
   useVerifyOTPMutation,
-  useResetPasswordMutation} = userAuthApi
+  useResetPasswordMutation,
+} = userAuthApi;
