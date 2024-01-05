d15796cb5c0278578fce3f30b79654f54309c64b
sanam <np03cs4m210078@heraldcollege.edu.np>
2024-01-05 22:59:34 +0545

    Edit the code to generate for random id.

--- backend/Airport_Security/utils.py
+++ backend/Airport_Security/utils.py
@@ -3,14 +3,6 @@ import os
 import random
 import string
 
-def generate_user_id():
-    # Generate a 4-digit random number
-    random_number = ''.join(random.choices(string.digits, k=4))
-    # Add "U" as a prefix to the random number
-    user_id = f'U{random_number}'
-    return user_id
-
-
 def generate_otp():
     characters = string.digits
     return ''.join(random.choice(characters) for _ in range(6))
