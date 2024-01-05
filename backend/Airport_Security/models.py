b05d71450f388cefd82100aa4e4e59d54171521b
sanam <np03cs4m210078@heraldcollege.edu.np>
2024-01-05 22:55:22 +0545

    Edit the model of User and Created the models for Staffs and Passengers.

--- backend/Airport_Security/models.py
+++ backend/Airport_Security/models.py
@@ -1,7 +1,19 @@
 from django.db import models
+import random
+import string
 from django.contrib.auth.models import BaseUserManager,AbstractBaseUser
-from .utils import generate_user_id
 from django.core.validators import RegexValidator
+
+def generate_user_id():
+    while True:
+        # Generate a 4-digit random number
+        random_number = ''.join(random.choices(string.digits, k=4))
+        # Add "U" as a prefix to the random number
+        user_id = f'U{random_number}'
+
+        # Check if the generated user_id already exists
+        if not User.objects.filter(user_id=user_id).exists():
+            return user_id
 
 class UserManager(BaseUserManager):
     def create_user(self, first_name, middle_name,  last_name, email, mobile_number, password=None, password2=None):
@@ -10,10 +22,10 @@ class UserManager(BaseUserManager):
             first_name=first_name,
             middle_name=middle_name,
             last_name=last_name,
-            mobile_number=mobile_number
-
+            mobile_number=mobile_number,
+            # role=role
         )
-       
+
         user.set_password(password)
         user.save(using=self._db)
         return user
@@ -32,7 +44,7 @@ class User(AbstractBaseUser):
         message='Mobile number must be 10 digits long.'
     )
 
-    user_id = models.CharField(max_length=5, primary_key=True, unique=True, default=generate_user_id())
+    user_id = models.CharField(max_length=5, primary_key=True, unique=True)
     first_name = models.CharField(max_length=50)
     middle_name = models.CharField(max_length=50, blank=True, null=True)
     last_name = models.CharField(max_length=50)
@@ -52,19 +64,81 @@ class User(AbstractBaseUser):
     status = models.CharField(max_length=8, choices=[('active', 'Active'), ('inactive', 'Inactive')], default='active')
     created_date = models.DateField(auto_now_add=True)
     created_time = models.TimeField(auto_now_add=True)
-    
-    
+
     objects = UserManager()
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['user_id','first_name', 'last_name','password','role','status']
+    REQUIRED_FIELDS = ['first_name', 'last_name','password','role','status']
 
-        
     def __str__(self):
         return self.user_id
+
+    def save(self, *args, **kwargs):
+        if not self.user_id:
+            self.user_id = generate_user_id()
+        super().save(*args, **kwargs)
+
+
+
+# for staff model
+class Staff(models.Model):
+    first_name = models.CharField(max_length=50)
+    middle_name = models.CharField(max_length=50, blank=True, null=True)
+    last_name = models.CharField(max_length=50)
+    mobile_regex = RegexValidator(
+        regex=r'^\d{10}$',  
+        message='Mobile number must be 10 digits long.'
+    )
+
+    email = models.EmailField(
+      verbose_name='Email',
+      max_length=255,
+      unique=True,
+    )
+    mobile_number = models.CharField(max_length=15,
+        blank=True, 
+        validators=[mobile_regex], 
+        )
     
+    address = models.CharField(max_length=50, blank=True, null=True)
+    department = models.CharField(max_length=50)
+    face_id = models.ForeignKey('Face', on_delete=models.CASCADE)
 
-       
-        
+# for face model
+class Face(models.Model):
+    face_id = models.CharField(max_length=6, primary_key=True, unique=True)
+    faces = models.ImageField(upload_to='face_images/')
 
-        
+# for passenger model
+class Passenger(models.Model):
+    first_name = models.CharField(max_length=50)
+    middle_name = models.CharField(max_length=50, blank=True, null=True)
+    last_name = models.CharField(max_length=50)
+    mobile_regex = RegexValidator(
+        regex=r'^\d{10}$',  
+        message='Mobile number must be 10 digits long.'
+    )
+
+    email = models.EmailField(
+      verbose_name='Email',
+      max_length=255,
+      unique=True,
+    )
+    mobile_number = models.CharField(max_length=15,
+        blank=True, 
+        validators=[mobile_regex], 
+        )
+    
+    flight_number = models.CharField(max_length=50)
+    plane_number = models.CharField(max_length=50)
+    booked_Date = models.DateField()
+    booked_Time = models.DateTimeField()
+    passport_number = models.CharField(max_length=50)
+    flight_Destination_from = models.CharField(max_length=50)
+    flight_Destination_to = models.CharField(max_length=50)
+    flight_Destination_Duration = models.DateTimeField()
+    depature_date = models.DateField()
+    depature_time = models.DateTimeField()
+    arrival_date = models.DateField()
+    arrival_time = models.DateTimeField()
+    
