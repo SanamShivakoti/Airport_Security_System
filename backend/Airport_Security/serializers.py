26b2d82e4b128fbf407794ac7eba96c4f4be5ddc
sanam <np03cs4m210078@heraldcollege.edu.np>
2024-01-05 22:56:30 +0545

    Fixed the errors for User registration.

--- backend/Airport_Security/serializers.py
+++ backend/Airport_Security/serializers.py
@@ -8,11 +8,10 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
     password2 = serializers.CharField(style={'input_type':'password'}, write_only=True)
     class Meta:
         model = User
-        fields = ['user_id','first_name','middle_name','last_name','email','mobile_number','password','password2']
+        fields = ['first_name','middle_name','last_name','email','role','mobile_number','password','password2']
         extra_kwargs={
             'password':{'write_only':True}
         }
-   
     
     def validate(self, attrs):
         password = attrs.get('password')
@@ -21,12 +20,10 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
             raise serializers.ValidationError("Password and Confirm Password doen't match.")
 
         return attrs
-    
-
 
     def create(self, validate_data):
+        print(validate_data)
         return User.objects.create_user(**validate_data)
-        
 
 # serializer for Login User
 class LoginUserSerializer(serializers.ModelSerializer):
