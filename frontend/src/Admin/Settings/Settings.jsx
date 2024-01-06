6ceffdb1a2837ec0e40c7549555b94cb17eee97e
sanam <np03cs4m210078@heraldcollege.edu.np>
2024-01-06 22:39:07 +0545

    feat added: Logged in User can update their profile.

--- frontend/src/Admin/Settings/Settings.jsx
+++ frontend/src/Admin/Settings/Settings.jsx
@@ -1,28 +1,31 @@
-import React, { useRef, useState, useEffect} from 'react';
-import { useNavigate } from 'react-router-dom';
-import { useAdminProfileViewQuery, useUpdateUserMutation } from '../../services/userAuthApi';
-import { getToken, storeToken } from '../../services/LocalStorageService';
+import React, { useRef, useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import {
+  useAdminProfileViewQuery,
+  useUpdateUserMutation,
+} from "../../services/userAuthApi";
+import { getToken, storeToken } from "../../services/LocalStorageService";
 import img from "./user.png";
-
 
 function Settings() {
   const inputRef = useRef(null);
-  const [server_error, setServerError] = useState({})
+  const [server_error, setServerError] = useState({});
   const formRef = useRef();
-  const [updateUser] = useUpdateUserMutation()
+  const [updateUser] = useUpdateUserMutation();
   const [image, setImage] = useState("");
-  const {access_token} = getToken();
-  const {data,isLoading, isError } = useAdminProfileViewQuery({access_token})
-  const handleImageClick = () =>{
+  const { access_token } = getToken();
+  const { data, refetch, isLoading, isError } = useAdminProfileViewQuery({
+    access_token,
+  });
+  const handleImageClick = () => {
     inputRef.current.click();
-  }
- 
-  const handleImageChange= (event) =>{
+  };
+
+  const handleImageChange = (event) => {
     const file = event.target.files[0];
     console.log(file);
     setImage(event.target.files[0]);
-    
-  }
+  };
 
   const [userData, setUserData] = useState({
     first_name: "",
@@ -35,7 +38,7 @@ function Settings() {
   });
 
   useEffect(() => {
-      
+    refetch();
     if (data) {
       setUserData({
         first_name: data.first_name || "",
@@ -43,59 +46,65 @@ function Settings() {
         last_name: data.last_name || "",
         email: data.email || "",
         mobile_number: data.mobile_number || "",
-        password: "",  
-        confirm_password: "", 
+        password: "",
+        confirm_password: "",
       });
     }
-  }, [data]); 
+  }, [data]);
 
   const { user_id } = data || {};
-  const handleSubmit= async (e)=>{
+  const handleSubmit = async (e) => {
     e.preventDefault();
     const data = new FormData(formRef.current);
     const actualData = {
-        first_name: data.get('first_name'),
-        middle_name: data.get('middle_name'),
-        last_name: data.get('last_name'),
-        email: data.get('email'),
-        mobile_number: data.get('mobile_number'),
-        password: "",
-        password2: ""
-        
+      first_name: data.get("first_name"),
+      middle_name: data.get("middle_name"),
+      last_name: data.get("last_name"),
+      email: data.get("email"),
+      mobile_number: data.get("mobile_number"),
+      password: "",
+      password2: "",
+      user_id,
+    };
+
+    const res = await updateUser({ user_id, actualData, access_token });
+
+    if (res.error) {
+      console.log(res.error.data.errors);
+      setServerError(res.error.data.errors);
     }
 
-    const res =  await updateUser({user_id, actualData, access_token})
-    
-    if(res.error){
-    console.log(res.error.data.errors)
-    setServerError(res.error.data.errors)
+    if (res.data) {
+      storeToken(res.data.token);
     }
-
-    if(res.data){
-
-    
-    storeToken(res.data.token)
-    }
-}
+  };
   const navigate = useNavigate();
-  
-  return(
+
+  return (
     <div>
-        <div className="text-3xl flex justify-center font-bold pb-2">
-          Settings
-        </div>
+      <div className="text-3xl flex justify-center font-bold pb-2">
+        Settings
+      </div>
       <div className="flex flex-col items-center ">
-           <p className="pb-2 text-2xl">Edit Profile</p>
-            <div onClick={handleImageClick}>
-              <div className="w-32 h-32 rounded-full  overflow-hidden"> 
-                {image ? <img src = {URL.createObjectURL(image)} alt="" /> : <img src = {img} alt="" />}
-                
-                <input type="file" ref={inputRef} onChange ={handleImageChange} style={{display:"none"}}/>
-              </div>
-            </div>
-          
-    </div>
-    <div className="mt-8 ">
+        <p className="pb-2 text-2xl">Edit Profile</p>
+        <div onClick={handleImageClick}>
+          <div className="w-32 h-32 rounded-full  overflow-hidden">
+            {image ? (
+              <img src={URL.createObjectURL(image)} alt="" />
+            ) : (
+              <img src={img} alt="" />
+            )}
+
+            <input
+              type="file"
+              ref={inputRef}
+              onChange={handleImageChange}
+              style={{ display: "none" }}
+            />
+          </div>
+        </div>
+      </div>
+      <div className="mt-8 ">
         <form ref={formRef} onSubmit={handleSubmit}>
           <div className="grid w-auto grid-cols-2 gap-4 laptop:px-40 desktop:px-52  tablet:px-32">
             <div className="mt-4">
@@ -113,7 +122,9 @@ function Settings() {
                 placeholder="First Name"
                 autoComplete="given-name"
                 value={userData.first_name}
-                onChange={(e) => setUserData({ ...userData, first_name: e.target.value })}
+                onChange={(e) =>
+                  setUserData({ ...userData, first_name: e.target.value })
+                }
                 className="block  my-px w-full m-0 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               />
             </div>
@@ -133,7 +144,9 @@ function Settings() {
                 placeholder="Middle Name"
                 autoComplete="given-name"
                 value={userData.middle_name}
-                onChange={(e) => setUserData({ ...userData, middle_name: e.target.value })}
+                onChange={(e) =>
+                  setUserData({ ...userData, middle_name: e.target.value })
+                }
                 className="block  my-px w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               />
             </div>
@@ -153,7 +166,9 @@ function Settings() {
                 placeholder="Last Name"
                 autoComplete="given-name"
                 value={userData.last_name}
-                onChange={(e) => setUserData({ ...userData, last_name: e.target.value })}
+                onChange={(e) =>
+                  setUserData({ ...userData, last_name: e.target.value })
+                }
                 className="block  my-px w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               />
             </div>
@@ -173,7 +188,9 @@ function Settings() {
                 placeholder="Email"
                 autoComplete="given-name"
                 value={userData.email}
-                onChange={(e) => setUserData({ ...userData, email: e.target.value })}
+                onChange={(e) =>
+                  setUserData({ ...userData, email: e.target.value })
+                }
                 className="block  my-px w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               />
             </div>
@@ -193,23 +210,23 @@ function Settings() {
                 placeholder="Mobile Number"
                 autoComplete="given-name"
                 value={userData.mobile_number}
-                onChange={(e) => setUserData({ ...userData, mobile_number: e.target.value })}
+                onChange={(e) =>
+                  setUserData({ ...userData, mobile_number: e.target.value })
+                }
                 className="block  my-px w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               />
             </div>
-
           </div>
 
           <div className="desktop:mt-24 laptop:mt-14 tablet:mt-14 flex items-center justify-end gap-x-6 laptop:px-40 desktop:px-52  tablet:px-32">
-            
-              <button
-                type="button"
-                onClick={()=> navigate('/Admin/Settings/verify/Otp/')}
-                className="rounded-md bg-indigo-600 w-[36rem] px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-              >
-                Change Password
-              </button>
-            
+            <button
+              type="button"
+              onClick={() => navigate("/Admin/Settings/verify/Otp/")}
+              className="rounded-md bg-indigo-600 w-[36rem] px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              Change Password
+            </button>
+
             <button
               type="submit"
               onClick={handleSubmit}
@@ -221,7 +238,7 @@ function Settings() {
         </form>
       </div>
     </div>
-  )
+  );
 }
 
 export default Settings;
