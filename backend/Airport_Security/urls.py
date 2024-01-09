14c42d1122ffa40b3dac7be2cde51a012dc722ba
sanam <np03cs4m210078@heraldcollege.edu.np>
2024-01-10 04:58:03 +0545

    Edited the some routes and setted the rout for displying flight details.

--- backend/Airport_Security/urls.py
+++ backend/Airport_Security/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from Airport_Security.views import UserRegistrationView, LoginUserView, AdminProfileView, OTPVerifiedPasswordResetView, UserView, UpdateUserView, DeleteUserView,VerifyOtpView, SendOtpResetEmailView, FilterUserView, AdminChangePasswordView, PassengerRegistrationView 
-from .views import open_camera
+from Airport_Security.views import UserRegistrationView, LoginUserView, UserProfileView, OTPVerifiedPasswordResetView, UserView, UpdateUserView, DeleteUserView,VerifyOtpView, SendOtpResetEmailView, FilterUserView, AdminChangePasswordView, PassengerRegistrationView 
+from .views import open_camera, flight_details_view
 
 urlpatterns = [
     path('register/user/', UserRegistrationView.as_view(), name = 'register'),
     path('login/', LoginUserView.as_view(), name='login'),
-    path('profile/', AdminProfileView.as_view(), name='profile'),
+    path('profile/', UserProfileView.as_view(), name='profile'),
     path('send_otp_email/', SendOtpResetEmailView.as_view(), name='send_otp_email'),
     path('otp_verification/', VerifyOtpView.as_view(), name='otp_verification'),
     path('users_list/', UserView.as_view(), name='users_list'),
@@ -13,8 +13,9 @@ urlpatterns = [
     path('user_delete/<str:user_id>/', DeleteUserView.as_view(), name='user_delete'),
     path('user_filter/<str:user_id>/', FilterUserView.as_view(), name='user_filter'),
     path('change_password/', AdminChangePasswordView.as_view(), name='change_password'),
-    path('open_camera/', open_camera, name='open_camera'), # Route for open camera
+    path('passport_scan/', open_camera, name='passport_scan'), # Route for open camera
     path('register/passenger/', PassengerRegistrationView.as_view(), name = 'register_passenger'),
+    path('flight_details/', flight_details_view.as_view(), name='flight_details'),
 
 
 ]
