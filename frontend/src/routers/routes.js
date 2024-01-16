859dc7575dbfce21a4f0076a6f4aeb489d8c1f8f
sanam <np03cs4m210078@heraldcollege.edu.np>
2024-01-17 00:20:40 +0545

    Set the routes for user login and flight details.

--- frontend/src/routers/routes.js
+++ frontend/src/routers/routes.js
@@ -1,3 +1,4 @@
+import React from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 // import for Admin control to setup router
 import Sidebar from "../Admin/Dashboard/Components";
@@ -5,8 +6,9 @@ import Sidebar from "../Admin/Dashboard/Components";
 import Usersidebar from "../Users/Dashboard/Components";
 // import for login page control to setup router
 import { Login } from "../login";
+import { UserLogin } from "../Users/login/login";
 import { useSelector } from "react-redux/es/hooks/useSelector";
-
+import PassengerDetails from "../flight_details";
 function PrivateRoute({ element, authenticated, redirectTo }) {
   return authenticated ? (
     element
@@ -31,7 +33,7 @@ function Url() {
           element={
             <PrivateRoute
               element={<Login />}
-              authenticated={!!isAuthenticated}
+              authenticated={!isAuthenticated}
               redirectTo="/Admin/dashboard/"
             />
           }
@@ -49,8 +51,15 @@ function Url() {
           }
         />
 
+        <Route path="/user/login/" element={<UserLogin />} />
+
         {/* Users controls route */}
         <Route path="/User/*" element={<Usersidebar />} />
+
+        <Route
+          path="/passenger/flight/details/"
+          element={<PassengerDetails />}
+        />
       </Routes>
     </BrowserRouter>
   );
