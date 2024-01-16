1bf7fa9cd3e468a256e215b3d23e428e7e9b8ca3
sanam <np03cs4m210078@heraldcollege.edu.np>
2024-01-17 00:36:03 +0545

    Fixed the error for web Socket connection.

--- backend/Airport_Security_System/asgi.py
+++ backend/Airport_Security_System/asgi.py
@@ -10,19 +10,17 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 import os
 import django
 
-
-
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Airport_Security_System.settings')
 django.setup()
+
 from django.urls import path
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from Airport_Security.consumers import PracticeConsumer
+
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
     "websocket": URLRouter([
-
             path('practice', PracticeConsumer.as_asgi())
         ])
 })
