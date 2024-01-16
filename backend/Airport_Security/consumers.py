d0604c30e01a1bcb325cef7dc1b0635d3165d9eb
sanam <np03cs4m210078@heraldcollege.edu.np>
2024-01-17 00:28:45 +0545

    Tested done for Web Socket Connection.

--- backend/Airport_Security/consumers.py
+++ backend/Airport_Security/consumers.py
@@ -2,10 +2,9 @@ from channels.generic.websocket import AsyncJsonWebsocketConsumer
 
 class PracticeConsumer(AsyncJsonWebsocketConsumer):
 
-      async def connect(self):
-           await self.accept()
+     async def connect(self):
+          await self.accept()
 
-      async def receive(self, text_data=None, bytes_data=None, **kwargs):
-            
-            if text_data == 'PING':
-                 await self.send('PONG')
+     async def receive(self, text_data=None, bytes_data=None, **kwargs):
+          print(text_data)
+          await self.send('PONG')
