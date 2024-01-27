ae35b685e72e3a3b44ccd67b45b8fb524e47c07c
sanam <np03cs4m210078@heraldcollege.edu.np>
2024-01-28 05:30:20 +0545

    feat: Registration of passengers by user.

--- frontend/src/Users/Passengers/Passengers_Registration.jsx
+++ frontend/src/Users/Passengers/Passengers_Registration.jsx
@@ -1,11 +1,86 @@
+import React, { useRef, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useRegisterPassengerMutation } from "../../services/userAuthApi";
+import { storeToken, getToken } from "../../services/LocalStorageService";
+
 function PassengersRegistration() {
+  const formRef = useRef();
+  const [server_error, setServerError] = useState({});
+  const [registerPassenger] = useRegisterPassengerMutation();
+  const { access_token } = getToken();
+
+  const formatToLocalISO = (date, time) => {
+    const localDateTime = new Date(`${date} ${time}`);
+    return localDateTime.toISOString();
+  };
+  const resetformFields = () => {
+    formRef.current.reset();
+  };
+  const handlePassengerRegister = async (e) => {
+    e.preventDefault();
+    const data = new FormData(formRef.current);
+    const actualData = {
+      first_name: data.get("first_name"),
+      middle_name: data.get("middle_name"),
+      last_name: data.get("last_name"),
+      email: data.get("email"),
+      mobile_number: data.get("mobile_number"),
+      flight_number: data.get("flight_number"),
+      plane_number: data.get("plane_number"),
+      booked_Date: data.get("booked_Date"),
+      booked_Time: data.get("booked_Time"),
+      passport_number: data.get("passport_number"),
+      flight_Destination_from: data.get("flight_Destination_from"),
+      flight_Destination_to: data.get("flight_Destination_to"),
+      depature_date: data.get("depature_date"),
+      depature_time: data.get("depature_time"),
+      arrival_date: data.get("arrival_date"),
+      arrival_time: data.get("arrival_time"),
+    };
+    actualData.booked_Date = new Date(actualData.booked_Date)
+      .toISOString()
+      .split("T")[0];
+    actualData.depature_date = new Date(actualData.depature_date)
+      .toISOString()
+      .split("T")[0];
+    actualData.arrival_date = new Date(actualData.arrival_date)
+      .toISOString()
+      .split("T")[0];
+
+    actualData.booked_Time = formatToLocalISO(
+      actualData.booked_Date,
+      actualData.booked_Time
+    );
+    actualData.depature_time = formatToLocalISO(
+      actualData.depature_date,
+      actualData.depature_time
+    );
+    actualData.arrival_time = formatToLocalISO(
+      actualData.arrival_date,
+      actualData.arrival_time
+    );
+
+    const res = await registerPassenger({ actualData, access_token });
+
+    if (res.error) {
+      console.log(res.error);
+      setServerError(res.error);
+    }
+
+    if (res.data) {
+      resetformFields();
+      storeToken(res.data.token);
+    }
+  };
+
+  const navigate = useNavigate();
   return (
     <div>
       <div className="text-3xl flex justify-center font-bold">
         Passengers Details
       </div>
       <div className="mt-6 ">
-        <form>
+        <form ref={formRef} onSubmit={handlePassengerRegister}>
           <div className="grid w-auto grid-cols-3 gap-4 laptop:px-32 desktop:px-40  tablet:px-24">
             <div className="mt-1">
               <label
@@ -17,7 +92,7 @@ function PassengersRegistration() {
 
               <input
                 type="text"
-                name="first-name"
+                name="first_name"
                 id="first-name"
                 placeholder="First Name"
                 autoComplete="given-name"
@@ -35,7 +110,7 @@ function PassengersRegistration() {
 
               <input
                 type="text"
-                name="middle-name"
+                name="middle_name"
                 id="middle-name"
                 placeholder="Middle Name"
                 autoComplete="given-name"
@@ -53,7 +128,7 @@ function PassengersRegistration() {
 
               <input
                 type="text"
-                name="last-name"
+                name="last_name"
                 id="last-name"
                 placeholder="Last Name"
                 autoComplete="given-name"
@@ -75,7 +150,7 @@ function PassengersRegistration() {
                 id="email"
                 placeholder="Email"
                 autoComplete="given-name"
-                className="block  my-px w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                className="block  my-px w-full rounded-md border-0 py-1.5 w-[36rem]text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               />
             </div>
 
@@ -89,7 +164,7 @@ function PassengersRegistration() {
 
               <input
                 type="number"
-                name="mobile"
+                name="mobile_number"
                 id="mobile"
                 placeholder="Mobile Number"
                 autoComplete="given-name"
@@ -113,7 +188,7 @@ function PassengersRegistration() {
 
               <input
                 type="text"
-                name="flight-number"
+                name="flight_number"
                 id="flight-number"
                 placeholder="Flight Number"
                 autoComplete="given-name"
@@ -131,7 +206,7 @@ function PassengersRegistration() {
 
               <input
                 type="text"
-                name="plane-number"
+                name="plane_number"
                 id="plane-number"
                 placeholder="Plane Number"
                 autoComplete="given-name"
@@ -149,7 +224,7 @@ function PassengersRegistration() {
 
               <input
                 type="date"
-                name="date"
+                name="booked_Date"
                 id="date"
                 placeholder="Date"
                 autoComplete="given-name"
@@ -167,7 +242,7 @@ function PassengersRegistration() {
 
               <input
                 type="time"
-                name="time"
+                name="booked_Time"
                 id="time"
                 placeholder="Time"
                 autoComplete="given-name"
@@ -185,7 +260,7 @@ function PassengersRegistration() {
 
               <input
                 type="text"
-                name="passport-number"
+                name="passport_number"
                 id="passport-number"
                 placeholder="Passport Number"
                 autoComplete="given-name"
@@ -203,7 +278,7 @@ function PassengersRegistration() {
             <div className="pr-2">
               <input
                 type="text"
-                name="from-destination"
+                name="flight_Destination_from"
                 id="from-destination"
                 placeholder="From"
                 autoComplete="given-name"
@@ -214,22 +289,11 @@ function PassengersRegistration() {
             <div className="pr-2">
               <input
                 type="text"
-                name="to-destination"
+                name="flight_Destination_to"
                 id="to-destination"
                 placeholder="To"
                 autoComplete="given-name"
                 className="block  my-px w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              />
-            </div>
-            <div className="pl-6 pr-2 py-1.5">Duration</div>
-            <div className="pr-2">
-              <input
-                type="time"
-                name="duration"
-                id="duration"
-                placeholder="Time"
-                autoComplete="given-name"
-                className="block  my-px w-auto rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               />
             </div>
           </div>
@@ -241,7 +305,7 @@ function PassengersRegistration() {
                 <div className="pr-2">
                   <input
                     type="date"
-                    name="date"
+                    name="depature_date"
                     id="date"
                     placeholder="Date"
                     autoComplete="given-name"
@@ -251,7 +315,7 @@ function PassengersRegistration() {
                 <div className="pr-2">
                   <input
                     type="time"
-                    name="time"
+                    name="depature_time"
                     id="time"
                     placeholder="Time"
                     autoComplete="given-name"
@@ -267,7 +331,7 @@ function PassengersRegistration() {
                 <div className="pr-2">
                   <input
                     type="date"
-                    name="date"
+                    name="arrival_date"
                     id="date"
                     placeholder="Date"
                     autoComplete="given-name"
@@ -277,7 +341,7 @@ function PassengersRegistration() {
                 <div className="pr-2">
                   <input
                     type="time"
-                    name="time"
+                    name="arrival_time"
                     id="time"
                     placeholder="Time"
                     autoComplete="given-name"
@@ -294,10 +358,10 @@ function PassengersRegistration() {
           </p>
 
           <div className="desktop:mt-7 laptop:mt-14 tablet:mt-14 flex items-center justify-end gap-x-6 laptop:px-32 desktop:px-40  tablet:px-24">
-
             <button
               type="submit"
-              className="rounded-md bg-lime-700 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-lime-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-lime-700"
+              onClick={handlePassengerRegister}
+              className="rounded-md bg-lime-700 w-[39rem] px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-lime-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-lime-700"
             >
               Register Passengers
             </button>
