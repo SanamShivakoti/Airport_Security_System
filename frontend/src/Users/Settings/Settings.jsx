39d551577dd3b036f6ebe350b3f160b2e5ae6bf0
sanam <np03cs4m210078@heraldcollege.edu.np>
2024-01-28 05:49:15 +0545

    feat: Added the User Setting for their profile update.

--- frontend/src/Users/Settings/Settings.jsx
+++ frontend/src/Users/Settings/Settings.jsx
@@ -1,40 +1,111 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import {
+  useAdminProfileViewQuery,
+  useUpdateUserProfileMutation,
+} from "../../services/userAuthApi";
+import { getToken, storeToken } from "../../services/LocalStorageService";
 import img from "./user.png";
 
-function UserSettings() {
-
+function Settings() {
   const inputRef = useRef(null);
+  const [server_error, setServerError] = useState({});
+  const formRef = useRef();
+  const [updateUser] = useUpdateUserProfileMutation();
   const [image, setImage] = useState("");
-
-  const handleImageClick = () =>{
+  const { access_token } = getToken();
+  const { data, refetch, isLoading, isError } = useAdminProfileViewQuery({
+    access_token,
+  });
+  const handleImageClick = () => {
     inputRef.current.click();
-  }
+  };
 
-  const handleImageChange= (event) =>{
+  const handleImageChange = (event) => {
     const file = event.target.files[0];
     console.log(file);
     setImage(event.target.files[0]);
-    
-  }
+  };
+
+  const [userData, setUserData] = useState({
+    first_name: "",
+    middle_name: "",
+    last_name: "",
+    email: "",
+    mobile_number: "",
+    password: "",
+    confirm_password: "",
+  });
+
+  useEffect(() => {
+    refetch();
+    if (data) {
+      setUserData({
+        first_name: data.first_name || "",
+        middle_name: data.middle_name || "",
+        last_name: data.last_name || "",
+        email: data.email || "",
+        mobile_number: data.mobile_number || "",
+        password: "",
+        confirm_password: "",
+      });
+    }
+  }, [data]);
+
+  const { user_id } = data || {};
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const data = new FormData(formRef.current);
+    const actualData = {
+      first_name: data.get("first_name"),
+      middle_name: data.get("middle_name"),
+      last_name: data.get("last_name"),
+      email: data.get("email"),
+      mobile_number: data.get("mobile_number"),
+      password: "",
+      password2: "",
+      user_id,
+    };
+
+    const res = await updateUser({ user_id, actualData, access_token });
+
+    if (res.error) {
+      console.log(res.error.data.errors);
+      setServerError(res.error.data.errors);
+    }
+
+    if (res.data) {
+      storeToken(res.data.token);
+    }
+  };
+  const navigate = useNavigate();
 
   return (
     <div>
-        <div className="text-3xl flex justify-center font-bold pb-2">
-          Settings
-        </div>
+      <div className="text-3xl flex justify-center font-bold pb-2">
+        Settings
+      </div>
       <div className="flex flex-col items-center ">
-           <p className="pb-2 text-2xl">Edit Profile</p>
-            <div onClick={handleImageClick}>
-              <div className="w-32 h-32 rounded-full  overflow-hidden"> 
-                {image ? <img src = {URL.createObjectURL(image)} alt="" /> : <img src = {img} alt="" />}
-                
-                <input type="file" ref={inputRef} onChange ={handleImageChange} style={{display:"none"}}/>
-              </div>
-            </div>
-          
-    </div>
-    <div className="mt-8 ">
-        <form>
+        <p className="pb-2 text-2xl">Edit Profile</p>
+        <div onClick={handleImageClick}>
+          <div className="w-32 h-32 rounded-full  overflow-hidden">
+            {image ? (
+              <img src={URL.createObjectURL(image)} alt="" />
+            ) : (
+              <img src={img} alt="" />
+            )}
+
+            <input
+              type="file"
+              ref={inputRef}
+              onChange={handleImageChange}
+              style={{ display: "none" }}
+            />
+          </div>
+        </div>
+      </div>
+      <div className="mt-8 ">
+        <form ref={formRef} onSubmit={handleSubmit}>
           <div className="grid w-auto grid-cols-2 gap-4 laptop:px-40 desktop:px-52  tablet:px-32">
             <div className="mt-4">
               <label
@@ -46,10 +117,14 @@ function UserSettings() {
 
               <input
                 type="text"
-                name="first-name"
+                name="first_name"
                 id="first-name"
                 placeholder="First Name"
                 autoComplete="given-name"
+                value={userData.first_name}
+                onChange={(e) =>
+                  setUserData({ ...userData, first_name: e.target.value })
+                }
                 className="block  my-px w-full m-0 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               />
             </div>
@@ -64,17 +139,21 @@ function UserSettings() {
 
               <input
                 type="text"
-                name="middle-name"
+                name="middle_name"
                 id="middle-name"
                 placeholder="Middle Name"
                 autoComplete="given-name"
+                value={userData.middle_name}
+                onChange={(e) =>
+                  setUserData({ ...userData, middle_name: e.target.value })
+                }
                 className="block  my-px w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               />
             </div>
 
             <div className="mt-4">
               <label
-                htmlFor="last-name"
+                htmlFor="last_name"
                 className="block ml-1 text-sm text-left font-medium leading-6 text-gray-900"
               >
                 Last name
@@ -82,10 +161,14 @@ function UserSettings() {
 
               <input
                 type="text"
-                name="last-name"
+                name="last_name"
                 id="last-name"
                 placeholder="Last Name"
                 autoComplete="given-name"
+                value={userData.last_name}
+                onChange={(e) =>
+                  setUserData({ ...userData, last_name: e.target.value })
+                }
                 className="block  my-px w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               />
             </div>
@@ -104,6 +187,10 @@ function UserSettings() {
                 id="email"
                 placeholder="Email"
                 autoComplete="given-name"
+                value={userData.email}
+                onChange={(e) =>
+                  setUserData({ ...userData, email: e.target.value })
+                }
                 className="block  my-px w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               />
             </div>
@@ -118,27 +205,31 @@ function UserSettings() {
 
               <input
                 type="number"
-                name="mobile"
+                name="mobile_number"
                 id="mobile"
                 placeholder="Mobile Number"
                 autoComplete="given-name"
+                value={userData.mobile_number}
+                onChange={(e) =>
+                  setUserData({ ...userData, mobile_number: e.target.value })
+                }
                 className="block  my-px w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               />
             </div>
-
           </div>
 
           <div className="desktop:mt-24 laptop:mt-14 tablet:mt-14 flex items-center justify-end gap-x-6 laptop:px-40 desktop:px-52  tablet:px-32">
-            <a href="Settings/verify/Otp">
-              <button
-                type="button"
-                className="rounded-md bg-indigo-600 w-[36rem] px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-              >
-                Change Password
-              </button>
-            </a>
+            <button
+              type="button"
+              onClick={() => navigate("/User/Settings/verify/Otp/")}
+              className="rounded-md bg-indigo-600 w-[36rem] px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              Change Password
+            </button>
+
             <button
               type="submit"
+              onClick={handleSubmit}
               className="rounded-md bg-lime-700 w-[36rem] px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-lime-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-lime-700"
             >
               Update Profile
@@ -150,4 +241,4 @@ function UserSettings() {
   );
 }
 
-export default UserSettings;
+export default Settings;
