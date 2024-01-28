3ee8199791cb2d906e9c3dbf41fec40b91a2479c
sanam <np03cs4m210078@heraldcollege.edu.np>
2024-01-28 05:51:57 +0545

    feat: Added for otp verification for password reset.

--- frontend/src/Users/Settings/change_password/verify_otp.jsx
+++ frontend/src/Users/Settings/change_password/verify_otp.jsx
@@ -1,34 +1,69 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-
+import {
+  useSendOTPMutation,
+  useVerifyOTPMutation,
+} from "../../../services/userAuthApi";
+import { getToken } from "../../../services/LocalStorageService";
+import { CircularProgress } from "@mui/material";
 function OtpVerification() {
   const [otp, setOtp] = useState("");
   const [verificationStatus, setVerificationStatus] = useState("");
+  const [loading, setLoading] = useState();
   const [otpSent, setOtpSent] = useState(false); // Track whether OTP has been sent
   const navigate = useNavigate();
+  const { access_token } = getToken();
 
+  const [sendotp, res] = useSendOTPMutation();
+  const [verify, data] = useVerifyOTPMutation();
+  const otpSend = async (access_token) => {
+    try {
+      const response = await sendotp({ access_token });
+
+      setOtpSent(true);
+      setVerificationStatus("OTP of 6 digits has been sent to your email.");
+    } catch (error) {}
+  };
   const handleOtpChange = (e) => {
     setOtp(e.target.value);
   };
 
-  const handleVerify = () => {
-    const mockOtp = "123456";
-    if (otp === mockOtp) {
-      setVerificationStatus("Verified!");
-      navigate("/User/Settings/set/newpassword");
-    } else {
-      setVerificationStatus("Incorrect OTP. Please try again.");
+  const handleVerify = async (access_token) => {
+    try {
+      const response = await verify({ access_token, otp: otp });
+
+      // const message = response.data.error;
+
+      if (response.error) {
+        if (response.error.status === 400) {
+          const otp_error = response.error.data.error;
+          setVerificationStatus(otp_error);
+        }
+      } else {
+        setVerificationStatus("Verified!");
+        navigate("/User/Settings/set/newpassword");
+      }
+    } catch (error) {
+      // Handle unexpected errors
+      console.error(error);
+      setVerificationStatus("Unexpected error. Please try again.");
     }
   };
 
-  const handleResendOtp = () => {
-    setOtpSent(true);
-    setVerificationStatus("OTP of 6 digits has been sent to your email.");
+  const handleResendOtp = async (access_token, res) => {
+    try {
+      const response = await sendotp({ access_token });
+
+      setOtpSent(true);
+      setVerificationStatus("OTP of 6 digits has been sent to your email.");
+    } catch (error) {}
   };
 
   return (
     <div className="max-w-md mx-auto">
-      <h1 className="text-3xl font-semibold text-center my-4">OTP Verification</h1>
+      <h1 className="text-3xl font-semibold text-center my-4">
+        OTP Verification
+      </h1>
       <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
         <div className="mb-4">
           <label className="block text-gray-700 text-sm font-bold mb-2">
@@ -41,17 +76,25 @@ function OtpVerification() {
               <input
                 type="text"
                 placeholder="Enter OTP"
+                name="otp"
                 value={otp}
                 onChange={handleOtpChange}
-                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:ring-2 focus:ring-inset focus:ring-indigo-600" 
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:ring-2 focus:ring-inset focus:ring-indigo-600"
               />
-              <p className="text-center text-blue-600 cursor-pointer underline mt-2" onClick={handleResendOtp}>
+              <p
+                className="text-center text-blue-600 cursor-pointer underline mt-2"
+                onClick={() => {
+                  handleResendOtp(access_token, res);
+                }}
+              >
                 Resend OTP
               </p>
             </>
           ) : (
             <button
-              onClick={handleResendOtp}
+              onClick={() => {
+                otpSend(access_token);
+              }}
               className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
             >
               Receive OTP
@@ -61,7 +104,9 @@ function OtpVerification() {
         {verificationStatus && (
           <div className="text-center">
             <button
-              onClick={handleVerify}
+              onClick={() => {
+                handleVerify(access_token);
+              }}
               className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
             >
               Verify
@@ -69,7 +114,9 @@ function OtpVerification() {
           </div>
         )}
         {verificationStatus && (
-          <p className="mt-4 text-center text-red-600">{verificationStatus}</p>
+          <p className="mt-4 text-center text-red-600">
+            {loading ? <CircularProgress /> : verificationStatus}
+          </p>
         )}
       </div>
     </div>
