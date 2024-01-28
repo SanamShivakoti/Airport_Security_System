d29b26f2f51175ec0d1e5a0993fe30535464dbc0
sanam <np03cs4m210078@heraldcollege.edu.np>
2024-01-28 05:45:58 +0545

    Edit and set routes for users auth.

--- frontend/src/Users/Dashboard/Components/userSidebar.jsx
+++ frontend/src/Users/Dashboard/Components/userSidebar.jsx
@@ -1,45 +1,62 @@
-import { Routes, Route } from "react-router-dom";
-import PassengerRegistration from "../../Passengers";
-import ViewPassengers from "../../View_passengers";
+import React from "react";
+import { Routes, Route, useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import PassengersRegistration from "../../Passengers";
+import UserViewPassengers from "../../View_passengers";
+import UserEditPassengers from "../../edit_passenger/user_edit_passenger";
 import UserSettings from "../../Settings";
 import OtpVerification from "../../Settings/change_password";
-import NewPasswordForm from "../../Settings/change_password/set_new_password";
+import UserNewPasswordForm from "../../Settings/change_password/set_new_password";
+import UserProfile from "../../user_profile/user_profile";
 import "./sidebar.css";
 import Content from "../Contents";
 import { sidebar_data } from "./sidebar_data";
+import { getToken } from "../../../services/LocalStorageService";
+import { useAdminProfileViewQuery } from "../../../services/userAuthApi";
 // import Avatar from "react-avatar";
 import img from "./user.png";
 function Usersidebar() {
+  const navigate = useNavigate();
+  const { access_token } = getToken();
+  const [userName, setUserName] = useState({});
+  const { data } = useAdminProfileViewQuery({ access_token });
+
+  useEffect(() => {
+    if (data) {
+      setUserName(data);
+    }
+  }, [data]);
+
   return (
     <div className="main-content">
       <div className="content">
         <div id="main-title">Airport Secuirty System</div>
         <div id="User-role-title">User</div>
-        <div className="profile">
+        <div className="profile" onClick={() => navigate("/User/profile/view")}>
           {/* <Avatar src="./user.png" /> */}
           <img id="profile-image" alt="" src={img} />
-          <div id="ProfileName">User Name</div>
+          <div id="ProfileName">{`${userName.first_name} ${userName.middle_name} ${userName.last_name}`}</div>
           <hr />
         </div>
 
         {/**Setting the sidebar clickable for render the selected items */}
         <ul className="SidebarList">
           {sidebar_data.map((val, key) => {
-           const isActive = window.location.pathname === val.link;
+            const isActive = window.location.pathname === val.link;
 
-           return (
-             <li
-               key={key}
-               className={`row ${isActive ? "active" : ""}`}
-               onClick={() => {
-                 window.location.pathname = val.link;
-               }}
-             >
-               {/**Display the icons and the name of the corresponding icons as title */}
-               <div id="icon">{val.icon}</div>
-               <div id="titlename">{val.title}</div>
-             </li>
-           );
+            return (
+              <li
+                key={key}
+                className={`row ${isActive ? "active" : ""}`}
+                onClick={() => {
+                  window.location.pathname = val.link;
+                }}
+              >
+                {/**Display the icons and the name of the corresponding icons as title */}
+                <div id="icon">{val.icon}</div>
+                <div id="titlename">{val.title}</div>
+              </li>
+            );
           })}
         </ul>
       </div>
@@ -50,11 +67,15 @@ function Usersidebar() {
         {/**User Passenger Registration route */}
         <Route
           path="/Passenger/Registration"
-          element={<PassengerRegistration />}
+          element={<PassengersRegistration />}
         />
 
         {/**User View Passengers route */}
-        <Route path="/View/Passengers" element={<ViewPassengers />} />
+        <Route path="/View/Passengers" element={<UserViewPassengers />} />
+        <Route
+          path="View/Passengers/Edit/:passenger_id"
+          element={<UserEditPassengers />}
+        />
 
         {/**User Setting Page route */}
         <Route path="/Settings" element={<UserSettings />} />
@@ -63,8 +84,12 @@ function Usersidebar() {
         <Route path="/Settings/verify/Otp" element={<OtpVerification />} />
 
         {/* User set new password form */}
-        <Route path="/Settings/set/newpassword" element={<NewPasswordForm />} />
-        
+        <Route
+          path="/Settings/set/newpassword"
+          element={<UserNewPasswordForm />}
+        />
+
+        <Route path="/profile/view" element={<UserProfile />} />
       </Routes>
     </div>
   );
