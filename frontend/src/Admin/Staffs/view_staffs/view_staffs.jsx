9180bddf10e59b33e6c62cd5e9177c4341d7565b
sanam <np03cs4m210078@heraldcollege.edu.np>
2024-01-31 21:29:55 +0545

    feat: Added to Display Staffs.

--- frontend/src/Admin/Staffs/view_staffs/view_staffs.jsx
+++ frontend/src/Admin/Staffs/view_staffs/view_staffs.jsx
@@ -1,219 +1,146 @@
-import React, {useState} from "react";
+import React, { useEffect, useMemo, useState } from "react";
+import { useDispatch } from "react-redux";
+import {
+  useGetStaffsQuery,
+  useDeleteStaffMutation,
+} from "../../../services/userAuthApi";
+import { getToken, removeToken } from "../../../services/LocalStorageService";
+import { removeUserToken } from "../../../features/authSlice";
+import { useNavigate } from "react-router-dom";
+function ViewStaffs() {
+  const [userIdSearch, setUserIdSearch] = useState("");
+  const { access_token } = getToken();
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
-function ViewStaffs(){
-    const [searchTerm, setSearchTerm] = useState();
+  const {
+    data: users = [],
+    error,
+    isLoading,
+    refetch,
+  } = useGetStaffsQuery({
+    access_token,
+  });
 
-    const handleSearchChange = (e) => {
-        setSearchTerm(e.target.value);
+  useEffect(() => {
+    refetch();
+  }, []);
+
+  const filteredUsers = useMemo(() => {
+    return users.filter(
+      (user) =>
+        user &&
+        user.staff_id &&
+        user.staff_id.toLowerCase().includes(userIdSearch.toLowerCase())
+    );
+  }, [users, userIdSearch]);
+
+  const [handleDelete, res] = useDeleteStaffMutation();
+  if (error) {
+    if (error.status === 401) {
+      dispatch(removeUserToken());
+      removeToken();
+      return navigate("/");
     }
+  }
 
-    const openCamera = () =>{
+  const handleSearchChange = (e) => {
+    setUserIdSearch(e.target.value);
+  };
 
-    }
-    
-    return(
-        <div>
-            <div className="text-3xl font-bold text-center">Staffs Details</div>
+  const deleteStaff = async (staff_id, access_token) => {
+    try {
+      const response = await handleDelete({ staff_id, access_token });
+      window.location.reload();
+    } catch (error) {}
+  };
 
-                {/* Search bar for passengers */}
-                <div className="mb-4 mt-10">
-                    {/* Search Input */}
-                    <input
-                        type="text"
-                        placeholder="Search by Staff ID"
-                        className="border rounded-md px-2 py-2 w-1/4" // Adjust width as needed
-                        value={searchTerm}
-                        onChange={handleSearchChange}
-                    />
+  const handleEditStaff = async (staff_id) => {
+    navigate(`Edit/${staff_id}`);
+  };
 
-                    {/* Search button */}
-                    <button
-                        type="button"
-                        className="rounded-md bg-blue-500 text-white px-2 py-2 ml-2 w-44" // Add margin-left for spacing
-                    >
-                        Search
-                    </button>
-                </div>
+  if (isLoading || res.isLoading) {
+    return <div>Loading...</div>;
+  }
 
-                <div className="mt-8 ">
-                    <form>
-                    <div className="grid w-auto grid-cols-2 gap-4 laptop:px-40 desktop:px-52  tablet:px-32">
-                        <div>
-                        <div className="mt-4">
-                            <label
-                            htmlFor="first-name"
-                            className="block ml-1 text-sm text-left font-medium leading-6 text-gray-900"
-                            >
-                            First name
-                            </label>
+  if (error) {
+    return <div>Error loading users: {error}</div>;
+  }
 
-                            <input
-                            type="text"
-                            name="first-name"
-                            id="first-name"
-                            placeholder="First Name"
-                            autoComplete="given-name"
-                            className="block  my-px w-full m-0 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                            />
-                        </div>
-
-                        <div className="mt-4">
-                            <label
-                            htmlFor="middle-name"
-                            className="block ml-1 text-sm text-left font-medium leading-6 text-gray-900"
-                            >
-                            Middle name
-                            </label>
-
-                            <input
-                            type="text"
-                            name="middle-name"
-                            id="middle-name"
-                            placeholder="Middle Name"
-                            autoComplete="given-name"
-                            className="block  my-px w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                            />
-                        </div>
-
-                        <div className="mt-4">
-                            <label
-                            htmlFor="last-name"
-                            className="block ml-1 text-sm text-left font-medium leading-6 text-gray-900"
-                            >
-                            Last name
-                            </label>
-
-                            <input
-                            type="text"
-                            name="last-name"
-                            id="last-name"
-                            placeholder="Last Name"
-                            autoComplete="given-name"
-                            className="block  my-px w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                            />
-                        </div>
-
-                        <div className="mt-4">
-                            <label
-                            htmlFor="email"
-                            className="block ml-1 text-sm text-left font-medium leading-6 text-gray-900"
-                            >
-                            Email
-                            </label>
-
-                            <input
-                            type="email"
-                            name="email"
-                            id="email"
-                            placeholder="Email"
-                            autoComplete="given-name"
-                            className="block  my-px w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                            />
-                        </div>
-
-                        <div className="mt-4">
-                            <label
-                            htmlFor="mobile no."
-                            className="block ml-1 text-sm text-left font-medium leading-6 text-gray-900"
-                            >
-                            Mobile No.
-                            </label>
-
-                            <input
-                            type="number"
-                            name="mobile"
-                            id="mobile"
-                            placeholder="Mobile Number"
-                            autoComplete="given-name"
-                            className="block  my-px w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                            />
-                        </div>
-
-                        <div className="mt-4">
-                            <label
-                            htmlFor="Address"
-                            className="block ml-1 text-sm text-left font-medium leading-6 text-gray-900"
-                            >
-                            Address
-                            </label>
-
-                            <input
-                            type="text"
-                            name="address"
-                            id="address"
-                            placeholder="Address"
-                            autoComplete="given-name"
-                            className="block  my-px w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                            />
-                        </div>
-
-                        <div className="mt-4">
-                            <label
-                                htmlFor="Department"
-                                className="block ml-1 text-sm text-left font-medium leading-6 text-gray-900"
-                            >
-                                Department
-                            </label>
-
-                            <select
-                            name="department"
-                            id="department"
-                            autoComplete="department-name"
-                            className="block  my-px px-2 w-full bg-white h-9 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                            >
-                            <option>Department-1</option>
-                            <option>Department-2</option>
-                            <option>Department-3</option>
-                            <option>Department-4</option>
-                            <option>Department-5</option>
-                            </select>
-                        </div>
-
-                        </div>
-                        {/* To display photo */}
-                        <div className="relative ">
-                            <div className="absolute top-0 right-0">
-                                <div className=" mt-4 px-4 bg-white w-60 h-60 mx-auto p-4 border-4 border-gray-300 rounded-md">
-                                <label
-                                    htmlFor="photo"
-                                    className="block ml-1 text-sm text-center font-medium leading-6 text-gray-900"
-                                >
-                                    Photo
-                                </label>
-                                </div>
-
-                                <button className="w-40 mt-4" onClick={openCamera}>
-                                Open Camera
-                                </button>
-                    
-                            </div>
-                        </div>
-                    </div>
-
-                    <p className="desktop:mt-16 laptop:mt-14 tablet:mt-14 flex text-base  laptop:px-40 desktop:px-52  tablet:px-32">
-                        Note: Staff ID will be auto generate by system. It will be a
-                        alphanumeric value of six characters.
-                    </p>
-
-                    <div className="desktop:mt-16 laptop:mt-14 tablet:mt-14 flex items-center justify-end gap-x-6 laptop:px-40 desktop:px-52  tablet:px-32">
-                        <button
-                            type="button"
-                            className="rounded-md bg-indigo-600 w-[36rem] px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                        >
-                            Update
-                        </button>
-
-                        <button
-                        type="submit"
-                        className="rounded-md bg-lime-700 w-[36rem] px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-lime-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-lime-700"
-                        >
-                        Delete Staffs
-                        </button>
-                    </div>
-                    </form>
-                </div>
+  return (
+    <div>
+      <div className="text-3xl flex justify-center font-bold pt-2">
+        View Staffs
+      </div>
+      <div className="pl-64 pr-2">
+        <div className="mb-4 mt-10">
+          {/* Search input */}
+          <input
+            type="text"
+            name="staff_id"
+            placeholder="Search by Passenger ID"
+            className="border rounded-md px-2 py-2 w-1/4"
+            value={userIdSearch}
+            onChange={handleSearchChange}
+          />
         </div>
-    )
+
+        <div className="overflow-x-auto overflow-y-auto">
+          <table className="min-w-full border-collapse table-auto">
+            <thead>
+              <tr>
+                <th className="px-1 py-2 w-1/12 text-left bg-gray-300">SN</th>
+                <th className="px-1 py-2 w-1/4 text-center bg-gray-300">
+                  Staff Name
+                </th>
+                <th className="px-1 py-2 w-1/6 text-center bg-gray-300">
+                  Staff ID
+                </th>
+                <th className="px-1 py-2 text-center bg-gray-300">Email</th>
+                <th className="px-1 py-2 text-center bg-gray-300">Edit</th>
+                <th className="px-1 py-2 text-center bg-gray-300">Delete</th>
+              </tr>
+            </thead>
+            <tbody>
+              {filteredUsers.map((user, index) => (
+                <tr
+                  key={user.staff_id}
+                  className="table-row group hover:bg-yellow-200 hover:cursor-pointer"
+                  data-row-id={user.id}
+                >
+                  <td className="border px-1 py-1 w-1/12">{index + 1}</td>
+                  <td className="border px-1 py-1 w-1/4">{`${user.first_name} ${user.middle_name} ${user.last_name}`}</td>
+                  <td className="border px-1 py-1 w-1/12">{user.staff_id}</td>
+                  <td className="border px-1 py-1 w-1/12">{user.email}</td>
+
+                  <td className="border px-1 py-1">
+                    <button
+                      type="button"
+                      className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                      onClick={() => handleEditStaff(user.staff_id)}
+                    >
+                      Edit
+                    </button>
+                  </td>
+                  <td className="border px-1 py-1">
+                    <button
+                      type="submit"
+                      className="rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600"
+                      onClick={() => {
+                        deleteStaff(user.staff_id, access_token);
+                      }}
+                    >
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default ViewStaffs;
