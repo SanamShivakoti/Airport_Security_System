4e6bef308f89e83c9a1cbbdd2dc5316c61df6db9
sanam <np03cs4m210078@heraldcollege.edu.np>
2024-02-04 11:08:09 +0545

    set the route for send mail to admin of unkown face.

--- backend/Airport_Security/urls.py
+++ backend/Airport_Security/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from Airport_Security.views import UserRegistrationView, LoginUserView, UserProfileView, OTPVerifiedPasswordResetView, UserView, UpdateUserView,UpdateUserProfileView, DeleteUserView,VerifyOtpView, SendOtpResetEmailView, FilterUserView, AdminChangePasswordView, PassengerRegistrationView, PassengerDetailView , PassengerView,UpdatePassengerView, DeletePassengerView, FilterPassengerView, StaffRegistrationView, StaffView, DeleteStaffView, FilterStaffView, UpdateStaffView, StaffDetailView
+from Airport_Security.views import UserRegistrationView, LoginUserView, UserProfileView, OTPVerifiedPasswordResetView, UserView, UpdateUserView,UpdateUserProfileView, DeleteUserView,VerifyOtpView, SendOtpResetEmailView, FilterUserView, AdminChangePasswordView, PassengerRegistrationView, PassengerDetailView , PassengerView,UpdatePassengerView, DeletePassengerView, FilterPassengerView, StaffRegistrationView, StaffView, DeleteStaffView, FilterStaffView, UpdateStaffView, StaffDetailView, SendMailToAdminView
 from .views import flight_details_view
 
 urlpatterns = [
@@ -27,5 +27,6 @@ urlpatterns = [
     path('staffs_filter/<str:staff_id>/', FilterStaffView.as_view(), name ='staffs_filter'),
     path('staffs_update/<str:staff_id>/', UpdateStaffView.as_view(), name='staffs_update'),
     path('staff/<str:face_id>/', StaffDetailView.as_view(), name='staff-detail'),
+    path('send_unknown_face/', SendMailToAdminView.as_view(), name='send_unknown_face')
 
 ]
