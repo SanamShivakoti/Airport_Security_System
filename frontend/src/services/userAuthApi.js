c1101d2e78c1d8ef00e0b7fe3466bce94ce4ad43
sanam <np03cs4m210078@heraldcollege.edu.np>
2024-02-14 09:39:43 +0545

    changes in base url.

--- frontend/src/services/userAuthApi.js
+++ frontend/src/services/userAuthApi.js
@@ -4,7 +4,9 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 // Define a service using a base URL and expected endpoints
 export const userAuthApi = createApi({
   reducerPath: "userAuthApi",
-  baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_API_URL }),
+  baseQuery: fetchBaseQuery({
+    baseUrl: `${process.env.REACT_APP_API_URL}/api/admin/`,
+  }),
   endpoints: (builder) => ({
     registerUser: builder.mutation({
       query: ({ actualData, access_token }) => {
